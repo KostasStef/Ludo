a60a81580c256ef648ab61198333eaea34771714
Anthony Nikolaidis <anthony.nikolaidis@infopelago.gr>
2021-01-12 22:14:52 +0200

    modifies '/' route to return the splash page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page */
-router.get("/game", function(req, res) {
-  res.sendFile("game.html", { root: "./public" });
+router.get("/", function (req, res) {
+    res.sendFile("splash.html", {root: "./public"});
 });
 
 /* GET home page */
-router.get("/", function(req, res) {
-  res.sendFile("game.html", { root: "./public" });
+router.get("/game", function (req, res) {
+    res.sendFile("game.html", {root: "./public"});
 });
 
 module.exports = router;
